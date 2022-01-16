68161ee1c82efd8c25a992d817c90c46460ff673
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-01-16 23:15:56 +0100

    pug dynamic include

--- fastify.js
+++ fastify.js
@@ -2,6 +2,7 @@
 
 const fp = require('fastify-plugin')
 const path = require('path')
+const pug = require('pug')
 
 // the use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
@@ -13,12 +14,35 @@ async function plugin (fastify, options = {}) {
   options = Object.assign(defaults, options)
 
   fastify.register(require('point-of-view'), {
+    propertyName: 'originalView',
     engine: { pug: require('pug') },
     root: options.views,
     options: {
       basedir: path.join(__dirname, '..') // "node_modules"
     }
   })
+
+  // https://stackoverflow.com/a/26525724/2389922
+  function view (template, options = {}) {
+    options.include = function pugDynamicIncludes (template) {
+      /* usage:
+        != include('template')
+      */
+      function render (file) {
+        const basedir = path.resolve('node_modules')
+        return pug.renderFile(file, Object.assign(options, { basedir }))
+      }
+      try {
+        return render(path.join('views', template + '.pug'))
+      } catch (error) {
+        return render(path.join('views', template, 'index.pug'))
+      }
+    }
+    return this.originalView(template, options)
+  }
+
+  fastify.decorateReply('view', view)
+  fastify.decorate('view', view)
 }
 
 module.exports = fp(plugin, { name: 'pug-material-design' })
