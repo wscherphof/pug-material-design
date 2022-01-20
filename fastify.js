16904d810d5548fe00a511c9570556e761ad9a6e
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-01-20 16:44:49 +0100

    new linter

--- fastify.js
+++ fastify.js
@@ -1,64 +1,64 @@
-'use strict'
+"use strict";
 
-const fp = require('fastify-plugin')
-const path = require('path')
-const pug = require('pug')
+const fp = require("fastify-plugin");
+const path = require("path");
+const pug = require("pug");
 
 // the use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 
-async function plugin (fastify, options = {}) {
+async function plugin(fastify, options = {}) {
   const defaults = {
-    views: './views',
-    propertyName: 'view'
-  }
-  options = Object.assign(defaults, options)
-  const { views, propertyName } = options
+    views: "./views",
+    propertyName: "view",
+  };
+  options = Object.assign(defaults, options);
+  const { views, propertyName } = options;
 
-  const unusedPropertyName = 'pugUnextended'
-  fastify.register(require('point-of-view'), {
+  const unusedPropertyName = "pugUnextended";
+  fastify.register(require("point-of-view"), {
     propertyName: unusedPropertyName,
-    engine: { pug: require('pug') },
+    engine: { pug },
     root: views,
     options: {
-      basedir: path.resolve('node_modules')
-    }
-  })
+      basedir: path.resolve("node_modules"),
+    },
+  });
 
-  function viewFunction (object, request) {
-    object = object || this
-    return function view (template, options = {}) {
-      options.request = request
-      options.include = function pugDynamicIncludes (template) {
+  function viewFunction(object, request) {
+    object = object || this;
+    return function view(template, options = {}) {
+      options.request = request;
+      options.include = function pugDynamicIncludes(template) {
         /* https://stackoverflow.com/a/26525724/2389922
           usage:
           != include('template')
         */
-        function render (file) {
-          options.basedir = path.resolve('node_modules')
-          return pug.renderFile(file, options)
+        function render(file) {
+          options.basedir = path.resolve("node_modules");
+          return pug.renderFile(file, options);
         }
         try {
-          return render(path.join(views, template + '.pug'))
+          return render(path.join(views, template + ".pug"));
         } catch (error) {
-          return render(path.join(views, template, 'index.pug'))
+          return render(path.join(views, template, "index.pug"));
         }
-      }
-      return object[unusedPropertyName](template, options)
-    }
+      };
+      return object[unusedPropertyName](template, options);
+    };
   }
 
-  fastify.decorateReply(propertyName, viewFunction())
-  fastify.decorate(propertyName, viewFunction())
+  fastify.decorateReply(propertyName, viewFunction());
+  fastify.decorate(propertyName, viewFunction());
 
-  function reDecorate (object, request) {
-    object[propertyName] = viewFunction(object, request)
+  function reDecorate(object, request) {
+    object[propertyName] = viewFunction(object, request);
   }
 
-  fastify.addHook('onRequest', async (request, reply) => {
-    reDecorate(reply, request)
-    reDecorate(fastify, request)
-  })
+  fastify.addHook("onRequest", async (request, reply) => {
+    reDecorate(reply, request);
+    reDecorate(fastify, request);
+  });
 }
 
-module.exports = fp(plugin, { name: 'pug-material-design' })
+module.exports = fp(plugin, { name: "pug-material-design" });
--- index.js
+++ index.js
@@ -1,45 +1,48 @@
-import './style.scss'
-import { MDCRipple } from '@material/ripple'
-import { MDCIconButtonToggle } from '@material/icon-button'
-import { MDCTextField } from '@material/textfield'
+import "./style.scss";
+import { MDCRipple } from "@material/ripple";
+import { MDCIconButtonToggle } from "@material/icon-button";
+import { MDCTextField } from "@material/textfield";
 
-function initMaterialDesign (element) {
-  function init (classes, init) {
-    classes = classes instanceof Array ? classes : [classes]
+function initMaterialDesign(element) {
+  function init(classes, init) {
+    classes = classes instanceof Array ? classes : [classes];
     classes.forEach((className) => {
       if (element.classList.contains(className)) {
-        init(element, className)
+        init(element, className);
       }
-      element.querySelectorAll('.' + className).forEach((element) => {
-        init(element, className)
-      })
-    })
+      element.querySelectorAll("." + className).forEach((element) => {
+        init(element, className);
+      });
+    });
   }
-  init(['mdc-button', 'mdc-icon-button'], (element, className) => {
-    const ripple = new MDCRipple(element)
-    if (className === 'mdc-icon-button') {
-      ripple.unbounded = true
-      if (element.getAttribute('toggle')) {
-        const iconToggle = new MDCIconButtonToggle(element)
-        iconToggle.listen('MDCIconButtonToggle:change', (event) => {
-          element.dispatchEvent(event)
-        })
+  init(["mdc-button", "mdc-icon-button"], (element, className) => {
+    const ripple = new MDCRipple(element);
+    if (className === "mdc-icon-button") {
+      ripple.unbounded = true;
+      if (element.getAttribute("toggle")) {
+        const iconToggle = new MDCIconButtonToggle(element);
+        iconToggle.listen("MDCIconButtonToggle:change", (event) => {
+          element.dispatchEvent(event);
+        });
       }
     }
-  })
-  init('mdc-text-field', (elt, className) => {
-    return new MDCTextField(elt)
-  })
+  });
+  init("mdc-text-field", (elt) => {
+    return new MDCTextField(elt);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  if (!htmx) { // eslint-disable-line
-    initMaterialDesign(document)
+document.addEventListener("DOMContentLoaded", function () {
+  // eslint-disable-next-line
+  if (!htmx) {
+    initMaterialDesign(document);
   }
-})
+});
 
-if (htmx) { // eslint-disable-line
-  htmx.on('htmx:load', function ({ detail }) { // eslint-disable-line
-    initMaterialDesign(detail.elt)
-  })
+// eslint-disable-next-line
+if (htmx) {
+  // eslint-disable-next-line
+  htmx.on("htmx:load", function ({ detail }) {
+    initMaterialDesign(detail.elt);
+  });
 }
