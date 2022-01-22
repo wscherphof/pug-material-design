147f939613abe8869d58135db6042af122e128aa
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-01-22 17:34:46 +0100

    script tags in pug

--- index.js
+++ index.js
@@ -3,46 +3,25 @@ import { MDCRipple } from '@material/ripple';
 import { MDCIconButtonToggle } from '@material/icon-button';
 import { MDCTextField } from '@material/textfield';
 
-function initMaterialDesign(element) {
-  function init(classes, init) {
-    classes = classes instanceof Array ? classes : [classes];
-    classes.forEach((className) => {
-      if (element.classList.contains(className)) {
-        init(element, className);
-      }
-      element.querySelectorAll('.' + className).forEach((element) => {
-        init(element, className);
-      });
+window['mdc-text-field'] = function (element) {
+  return new MDCTextField(element);
+};
+
+function ripple(element) {
+  return new MDCRipple(element);
+}
+
+window['mdc-button'] = function (element) {
+  ripple(element);
+};
+
+window['mdc-icon-button'] = function (element) {
+  const ripple = ripple(element);
+  ripple.unbounded = true;
+  if (element.getAttribute('toggle')) {
+    const iconToggle = new MDCIconButtonToggle(element);
+    iconToggle.listen('MDCIconButtonToggle:change', (event) => {
+      element.dispatchEvent(event);
     });
   }
-  init(['mdc-button', 'mdc-icon-button'], (element, className) => {
-    const ripple = new MDCRipple(element);
-    if (className === 'mdc-icon-button') {
-      ripple.unbounded = true;
-      if (element.getAttribute('toggle')) {
-        const iconToggle = new MDCIconButtonToggle(element);
-        iconToggle.listen('MDCIconButtonToggle:change', (event) => {
-          element.dispatchEvent(event);
-        });
-      }
-    }
-  });
-  init('mdc-text-field', (elt) => {
-    return new MDCTextField(elt);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  // eslint-disable-next-line
-  if (!htmx) {
-    initMaterialDesign(document);
-  }
-});
-
-// eslint-disable-next-line
-if (htmx) {
-  // eslint-disable-next-line
-  htmx.on('htmx:load', function ({ detail }) {
-    initMaterialDesign(detail.elt);
-  });
-}
+};
