fe16b1d7a23eb0afb87b1d9a8668d9712309ee4c
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-01-25 19:00:08 +0100

    0.3.5 add checkbox

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ import './style.scss';
 import { MDCRipple } from '@material/ripple';
 import { MDCIconButtonToggle } from '@material/icon-button';
 import { MDCTextField } from '@material/textfield';
-
-window['mdc-text-field'] = function (element) {
-  return new MDCTextField(element);
-};
+import { MDCFormField } from '@material/form-field';
+import { MDCCheckbox } from '@material/checkbox';
 
 function ripple(element) {
   return new MDCRipple(element);
@@ -21,4 +19,15 @@ window['mdc-icon-button'] = function (element) {
   if (element.getAttribute('toggle')) {
     return new MDCIconButtonToggle(element);
   }
+};
+
+window['mdc-text-field'] = function (element) {
+  return new MDCTextField(element);
+};
+
+window['mdc-checkbox'] = function (element) {
+  element = element.parentElement;
+  const checkbox = new MDCCheckbox(element);
+  const formField = new MDCFormField(element.closest('.mdc-form-field'));
+  formField.input = checkbox;
 };
